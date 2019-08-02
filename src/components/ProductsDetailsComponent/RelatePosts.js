ac96a2756b13574a314fa310cf3fabb2f8cfd0c3
villegas-06 <villegas4006@gmail.com>
2019-08-02 12:29:04 -0500

    Articulos Relacionados (Destacados)

--- src/components/ProductsDetailsComponent/RelatePosts.js
+++ src/components/ProductsDetailsComponent/RelatePosts.js
@@ -5,90 +5,102 @@ import Lampara from './lamparita.png';
 
 class RelatePosts extends React.Component{
     render(){
-        return(
-            <div class="container">
-                <div class="container">
-                    <div>
-                        OTHER PRODUCTS IN THE SAME CATEGORY:
-                    </div>
+        return( 
+
+        <div className="container">
+        <div className="container">
+                <h4>
+                    <b>REVIEWS</b>
+                </h4>
+
+                
+                <button type="button" className="btn btn-warning mb-3 mt-2">WIRTE YOUR REVIEW</button>
+
+                <p>No customize reviews for the moment</p>
+        
+                <div className="prod_det_alert">
+                    OTHER PRODUCTS IN THE SAME CATEGORY:
                 </div>
-                <div class="container">
-                        <div class="row">
-                            <div class="col-3">
-                                <div class="card prod_det_art_relacionados">
+                <div>
+                    <div className="row">
+                        <div className="col-3">
+                            <div className="card prod_det_art_relacionados">
+                            <p>New</p>
+                            <div className="container prod_det_img">
+                            <img src={Lampara} className="card-img-top" alt="..."/>
+                            </div>
+                            <div className="container card-body prod_det_card_body">
+                            <div className="ec-stars-wrapper">
+                            <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
+                            <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
+                            <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
+                            <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
+                            <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
+                            </div>
+                            <p className="card-text">
+                                Night Lamp
+                            </p>
+                            <b>
+                                80.000
+                            </b>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="card prod_det_art_relacionados">
                                 <p>New</p>
-                                <div class="container prod_det_img">
-                                <img src={Lampara} class="card-img-top" alt="..."/>
+                                <div className="container prod_det_img">
+                                <img src={Lampara} className="card-img-top" alt="..."/>
                                 </div>
-                                <div class="container card-body prod_det_card_body">
-                                <div class="ec-stars-wrapper">
+                                <div className="container card-body prod_det_card_body">
+                                <div className="ec-stars-wrapper">
                                 <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
                                 <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
                                 <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
                                 <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
                                 <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
                                 </div>
-                                <p class="card-text">
+                                <p className="card-text">
+                                Night Lamp
+                                </p>
+                                                
+                                <b>
+                                80.000
+                                </b>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="card prod_det_art_relacionados">
+                                <p>New</p>
+                                <div className="container prod_det_img">
+                                <img src={Lampara} className="card-img-top" alt="..."/>
+                                </div>
+                                <div className="container card-body prod_det_card_body">
+                                <div className="ec-stars-wrapper">
+                                <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
+                                <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
+                                <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
+                                <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
+                                <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
+                                </div>
+                                <p className="card-text">
                                     Night Lamp
                                 </p>
+                                                
                                 <b>
                                     80.000
                                 </b>
                                 </div>
-                                </div>
+                             </div>
                             </div>
-                            <div class="col-3">
-                                <div class="card prod_det_art_relacionados">
-                                    <p>New</p>
-                                    <div class="container prod_det_img">
-                                    <img src={Lampara} class="card-img-top" alt="..."/>
-                                    </div>
-                                    <div class="container card-body prod_det_card_body">
-                                    <div class="ec-stars-wrapper">
-                                    <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
-                                    <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
-                                    <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
-                                    <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
-                                    <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
-                                    </div>
-                                    <p class="card-text">
-                                    Night Lamp
-                                    </p>
-                                                    
-                                    <b>
-                                    80.000
-                                    </b>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="card prod_det_art_relacionados">
-                                    <p>New</p>
-                                    <div class="container prod_det_img">
-                                    <img src={Lampara}   class="card-img-top" alt="..."/>
-                                    </div>
-                                    <div class="container card-body prod_det_card_body">
-                                    <div class="ec-stars-wrapper">
-                                    <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
-                                    <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
-                                    <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
-                                    <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
-                                    <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
-                                    </div>
-                                    <p class="card-text">
-                                        Night Lamp
-                                    </p>
-                                                    
-                                    <b>
-                                        80.000
-                                    </b>
-                                    </div>
-                                 </div>
-                                </div>
-                </div>
             </div>
         </div>
-        
+    </div>
+
+    </div>
+
+
         );
     }
 }
