6a7eaa33467224a218eb593a24984d33f710f2e6
villegas-06 <villegas4006@gmail.com>
2019-08-02 17:14:51 -0500

    Product Details Component(Incompleto)

--- src/components/ProductsDetailsComponent/ProductsDetailsComponent.js
+++ src/components/ProductsDetailsComponent/ProductsDetailsComponent.js
@@ -3,30 +3,30 @@ import './ProductDetailsComponent.css'
 import Sofa from './sofa.png'
 const Product_Details = () =>{
 
-<div class="contianer">
-        <div class="container">
+<div className="contianer">
+        <div className="container">
             <h6>Home/Furniture/Sofas/Huch/Pandora Sofa</h6>
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-6">
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
                     <br/>
-                    <p class="prod_det_p">New</p>
-                    <div class="container prod_det_img">
-                    <img class="prod_det_img" src={Sofa} alt="Sofá"/>
+                    <p className="prod_det_p">New</p>
+                    <div className="container prod_det_img">
+                    <img className="prod_det_img" src={Sofa} alt="Sofá"/>
                     </div>
                 </div>
-                <div class="col-6">
+                <div className="col-6">
                     <b>Pandora Sofa</b>
                     <p style="margin-top: 15px;">180.000</p>
                     <p>Printed evening dress width straights sleeves with black than 
                         waist bel and ruffed things.</p>
-                        <div class="row">
-                            <div class="col-3">
+                        <div className="row">
+                            <div className="col-3">
                                  <b>Size</b>
                             </div>
-                            <div class="col-4">
-                                <select class="prod_det_size" name="transporte">
+                            <div className="col-4">
+                                <select className="prod_det_size" name="transporte">
                                     <option value="1">S</option>
                                     <option value="2">M</option>
                                     <option value="3">L</option>
@@ -35,32 +35,32 @@ const Product_Details = () =>{
                             </div>
                             </div>
                             
-                        <div class="row prod_det_mt" >
-                                <div class="col-3">
+                        <div className="row prod_det_mt" >
+                                <div className="col-3">
                                     <b>Color</b>
                                 </div>
                                 
-                                <div class="col-2">
-                                    <table class="prod_det_box_1"> 
+                                <div className="col-2">
+                                    <table className="prod_det_box_1"> 
                                      <th  scope="col"></th>
                                     </table>
                                 </div>
 
-                                <div class="col-2">
+                                <div className="col-2">
                                     
-                                <table class="prod_det_box_2 ml-60"> 
+                                <table className="prod_det_box_2 ml-60"> 
                                         <th scope="col"></th>
                                 </table>
 
                                 </div>
                                 </div>
 
-                        <div class="row prod_det_mt">
-                            <div class="col-3">
+                        <div className="row prod_det_mt">
+                            <div className="col-3">
                                 <b>Quantify</b>
                             </div>
-                            <div class="col-4">
-                                <select class="prod_det_size" name="transporte">
+                            <div className="col-4">
+                                <select className="prod_det_size" name="transporte">
                                     <option value="1">1</option>
                                     <option value="2">2</option>
                                     <option value="3">3</option>
