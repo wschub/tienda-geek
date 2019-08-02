684c3720ab6afcbd24f0fc56c0d77946126dc91b
Sala 310 <sala310@poliacademico.edu.loc>
2019-08-02 10:34:25 -0500

    base-proyecto

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Layout from '../components/Partials/LayoutComponent';
+
 
 
 
 function App() {
   return (
   <React.Fragment>
+   <Layout> 
      
+   </Layout>
+    
   </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import App from './components/App';
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
