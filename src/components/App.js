b206b679b6ad5819f0098c9fe471bf39cae6a3da
Sala 310 <brahianestiven08@gmail.com>
2019-08-02 11:02:38 -0500

    Menu de opciones y submenu de productos

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Layout from '../components/Partials/LayoutComponent';
 
-
-
-
 function App() {
   return (
   <React.Fragment>
    <Layout> 
-     
+    
    </Layout>
     
   </React.Fragment>
