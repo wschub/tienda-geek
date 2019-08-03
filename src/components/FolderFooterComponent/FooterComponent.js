dbc87408baa41a3d12514a3bc69bb774f5c7f7ad
JuanZuleta <juanzuleta@localhost.localdomain>
2019-08-03 17:14:56 -0500

    Footer Listo

--- src/components/FolderFooterComponent/FooterComponent.js
+++ src/components/FolderFooterComponent/FooterComponent.js
@@ -5,8 +5,8 @@ import './Footer.css'
 const Footer = () => {
     return (
         <React.Fragment>
-            <div className="container-fluid">
-                <div className='row container text-center m-0'>
+            <div className="container-fluid text-center mt-5">
+                <div className='row'>
                     <ul className='col-lg-3 col-sm-6 col-md-6 col-xs-12'>
                         <h6 className='mb-3'>PRODUCTS</h6>
                         <li><Link to='/'>Prices drop</Link></li>
@@ -41,31 +41,31 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <div className='row container text-center my-5 '>
-                <div className='mx-5 col-6 '>
+            <div className="container">
+            <div className='row text-center my-5'>
+                <div className='col-md-6'>
                     <h5>Download Our App</h5>
                 </div>                
-                <div className='mx-5 col-6'>
-                    <i className="fab fa-apple ml-5 fa-2x"></i>
+                <div className='col-md-6'>
+                    <i className="fab fa-apple fa-2x"></i>
                     <i className="fab fa-google-play ml-5 fa-2x"></i>
                     <i className="fab fa-windows ml-5 fa-2x"></i>
                 </div>
 
             </div>
+            </div>
 
-            <div className="text-white bg-dark w-100 m-0" role="alert">
-                <div className='container row'>
-                    <div className="col-6 text-center"> <h6> @ Copyright 2019 Academia Geek</h6></div>
-                    <div className="d-flex col-6 justify-content-end">
+            <div className="text-white bg-dark text-center" role="alert">
+                <div className='row m-0'>
+                    <div className="col-md-6"> <h6> @ Copyright 2019 Academia Geek</h6></div>
+                    <div className="col-md-6">
                         <i className="fab fa-2x fa-cc-paypal ml-2 icono2"></i>
                         <i className="fab fa-2x fa-cc-mastercard ml-2 icono2"></i>
                         <i className="fab fa-2x fa-cc-mastercard ml-2 icono2"></i>
                         <i className="fab fa-2x fa-cc-visa ml-2 icono2"></i>
                     </div>
                 </div>
-
             </div>
-
         </React.Fragment>
     )
 }
