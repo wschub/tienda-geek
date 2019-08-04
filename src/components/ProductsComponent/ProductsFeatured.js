01dc26e907e9d682333e0d64f48e232a980294d6
Isaza156 <jonathan.isaza@hotmail.com>
2019-08-03 22:38:46 -0500

    actualizacion

--- src/components/ProductsComponent/ProductsFeatured.js
+++ src/components/ProductsComponent/ProductsFeatured.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Productstyle.css';
-import 'font-awesome/css/font-awesome.min.css';
-
-
 
 class Product extends React.Component {
 
